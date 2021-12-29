d75809cad14a04cf0986e606ad30985b7ae92a5e
narumichikubo <kubo@arsaga.jp>
2021-12-29 19:00:20 +0900

    sdk versionの調整

--- buildSrc/src/main/java/dependencies/BuildConfig.kt
+++ buildSrc/src/main/java/dependencies/BuildConfig.kt
@@ -8,9 +8,9 @@ object BuildConfig {
     const val applicationId = "com.narumichi.architecuturetemplate"
     const val versionCode = 1
     const val versionName = "1.0"
-    const val targetSdkVersion = 31
-    const val compileSdkVersion = 29
-    const val minSdkVersion = 30
+    const val targetSdkVersion = 30
+    const val compileSdkVersion = 31
+    const val minSdkVersion = 26
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     const val consumerProguardFiles = "consumer-rules.pro"
 }
