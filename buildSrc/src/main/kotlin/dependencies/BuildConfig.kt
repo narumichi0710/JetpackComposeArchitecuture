587f61ea5b12ea82b19b10018ba5ef260e3c1638
narumichikubo <kubo@arsaga.jp>
2021-12-30 19:23:37 +0900

    ビルドスクリプトをscript packageに切り分け

---
+++ buildSrc/src/main/kotlin/dependencies/BuildConfig.kt
@@ -0,0 +1,12 @@
+package dependencies
+
+object BuildConfig {
+    const val applicationId = "com.narumichi.architecuturetemplate"
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val targetSdkVersion = 30
+    const val compileSdkVersion = 31
+    const val minSdkVersion = 26
+    const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    const val consumerProguardFiles = "consumer-rules.pro"
+}
---
+++ buildSrc/src/main/kotlin/script/StaticScript.kt
@@ -0,0 +1,66 @@
+package script
+
+import com.android.build.gradle.BaseExtension
+import dependencies.BuildConfig
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+
+
+object StaticScript {
+
+    fun baseExtension(
+        baseExtension: BaseExtension,
+        isRoot: Boolean = false,
+        project: Project? = null,
+    ) {
+        provideDefaultConfig(baseExtension, isRoot)
+        provideBaseExtension(baseExtension)
+        provideBuildSetting(baseExtension)
+    }
+
+    private fun provideDefaultConfig(
+        baseExtension: BaseExtension,
+        isRoot: Boolean,
+    ) {
+        baseExtension.apply {
+            defaultConfig {
+                if (isRoot) this.applicationId = BuildConfig.applicationId
+                minSdk = BuildConfig.minSdkVersion
+                targetSdk = BuildConfig.targetSdkVersion
+                versionCode = BuildConfig.versionCode
+                versionName = BuildConfig.versionName
+
+                testInstrumentationRunner = BuildConfig.testInstrumentationRunner
+
+                compileSdkVersion(BuildConfig.compileSdkVersion)
+                consumerProguardFiles(BuildConfig.consumerProguardFiles)
+            }
+        }
+    }
+
+    private fun provideBaseExtension(baseExtension: BaseExtension) {
+        baseExtension.apply {
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_1_8
+                targetCompatibility = JavaVersion.VERSION_1_8
+            }
+            lintOptions {
+                isAbortOnError = true
+            }
+        }
+    }
+
+    private fun provideBuildSetting(baseExtension: BaseExtension) {
+        baseExtension.apply {
+            buildTypes {
+                getByName("release") {
+                    isMinifyEnabled = false
+                    proguardFiles(
+                        getDefaultProguardFile("proguard-android-optimize.txt"),
+                        "proguard-rules.pro"
+                    )
+                }
+            }
+        }
+    }
+}
