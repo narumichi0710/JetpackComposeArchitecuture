35e96604042945712873d2844d76cd08bbbbfaae
narumichikubo <narumichi7373@gmail.com>
2022-01-03 21:57:30 +0900

    feat: 🎸 Ability to generate singletons by Hilt in repoistory, useCase, entity

---
+++ domain/entity/core/src/main/java/com/narumichi/domain/entity/core/di/EntityModule.kt
@@ -0,0 +1,10 @@
+package com.narumichi.domain.entity.core.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+object EntityModule {
+}
