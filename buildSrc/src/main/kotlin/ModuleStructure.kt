aa40c70fa06745625f3de06cbde75c6bf769e150
narumichikubo <narumichi7373@gmail.com>
2022-01-21 01:18:43 +0900

    rrefactor: 💡 Move the ModuleStructure function to script

--- buildSrc/src/main/kotlin/ModuleStructure.kt
+++ buildSrc/src/main/kotlin/ModuleStructure.kt
@@ -1,16 +1,15 @@
 import ModuleStructure.configureModuleStructure
-import ModuleStructure.findModuleType
-
+import script.ModuleExtension
 import org.gradle.api.Project
-import org.gradle.api.artifacts.dsl.DependencyHandler
-import script.Libs
+import script.ModuleExtension.api
+import script.ModuleExtension.impl
 
 /**
  * モジュール間の依存関係を定義するオブジェクト
  */
 
 fun Project.baseModuleStructure() {
-    findModuleType(this)?.let {
+    ModuleExtension.findModuleType(this)?.let {
         configureModuleStructure(this, it)
     }
 }
@@ -23,7 +22,7 @@ object ModuleStructure {
     ) = project.dependencies.apply {
         when (moduleType) {
             ProjectModule.ModuleType._app -> {
-                implAllModule(this, ProjectModule.ModuleType._app)
+                ModuleExtension.implAllModule(this, ProjectModule.ModuleType._app)
             }
             ProjectModule.ModuleType._presentation_view_core -> {
                 api(ProjectModule.ModuleType._presentation_viewModel_core)
@@ -41,71 +40,8 @@ object ModuleStructure {
                 api(ProjectModule.ModuleType._data_gateway_remote)
                 api(ProjectModule.ModuleType._data_gateway_local)
             }
-            else -> implDomainModule(this, moduleType)
+            else -> ModuleExtension.implDomainModule(this, moduleType)
         }
     }
 
-
-    internal fun findModuleType(project: Project): ProjectModule.ModuleType? {
-        val projectName = convertEnumName(project)
-        return ProjectModule.ModuleType.values()
-            .find { projectName == it.name }
-    }
-
-    internal fun implDomainModule(
-        dependencyHandler: DependencyHandler,
-        moduleType: ProjectModule.ModuleType?
-    ) {
-        sameLayerCoreModule(moduleType).forEach {
-            dependencyHandler.api(it)
-        }
-        sameLayerChildModule(moduleType).forEach {
-            dependencyHandler.api(it)
-        }
-    }
-
-    private fun sameLayerCoreModule(
-        moduleType: ProjectModule.ModuleType?
-    ): List<ProjectModule.ModuleType> = ProjectModule.ModuleType.values().filter {
-        it.name.endsWith("_core") && moduleType != it && moduleType!!.name?.startsWith(
-            it?.name?.substringBefore(
-                "_core"
-            )
-        )
-    }
-
-    private fun sameLayerChildModule(
-        moduleType: ProjectModule.ModuleType?
-    ): List<ProjectModule.ModuleType> = ProjectModule.ModuleType.values()
-        .filter {
-            moduleType != it && it.name?.startsWith(moduleType!!.name)
-        }
-
-    private fun convertEnumName(project: Project): String = project.path
-        .replace(":", "_")
-
-    private fun convertModulePath(moduleType: ProjectModule.ModuleType): String = moduleType.name
-        .replace("_", ":")
-
-    internal fun implAllModule(
-        dependencyHandler: DependencyHandler,
-        moduleType: ProjectModule.ModuleType
-    ) = ProjectModule.ModuleType.values().toSet()
-        .minus(moduleType).forEach {
-            dependencyHandler.impl(it)
-        }
-
-    internal fun DependencyHandler.impl(moduleType: ProjectModule.ModuleType) {
-        convertModulePath(moduleType).let { modulePath ->
-            println("structure:impl => $modulePath")
-            add(Libs.Props.Impl.prop, project(mapOf("path" to modulePath)))
-        }
-    }
-
-    internal fun DependencyHandler.api(moduleType: ProjectModule.ModuleType) {
-        convertModulePath(moduleType).let { modulePath ->
-            println("structure:api => $modulePath")
-            add(Libs.Props.Api.prop, project(mapOf("path" to modulePath)))
-        }
-    }
 }
--- buildSrc/src/main/kotlin/script/ModuleExtension.kt
+++ buildSrc/src/main/kotlin/script/ModuleExtension.kt
@@ -1,4 +1,72 @@
 package script
 
-class ModuleExtension {
+import org.gradle.api.Project
+import org.gradle.api.artifacts.dsl.DependencyHandler
+
+object ModuleExtension {
+
+    internal fun findModuleType(project: Project): ProjectModule.ModuleType? {
+        val projectName = convertEnumName(project)
+        return ProjectModule.ModuleType.values()
+            .find { projectName == it.name }
+    }
+
+    internal fun implDomainModule(
+        dependencyHandler: DependencyHandler,
+        moduleType: ProjectModule.ModuleType?
+    ) {
+        moduleType?.let {
+            sameLayerCoreModule(it).forEach {
+                dependencyHandler.api(it)
+            }
+            sameLayerChildModule(it).forEach {
+                dependencyHandler.api(it)
+            }
+        }
+    }
+
+    internal fun sameLayerCoreModule(
+        moduleType: ProjectModule.ModuleType
+    ): List<ProjectModule.ModuleType> = ProjectModule.ModuleType.values().filter {
+        it.name.endsWith("_core") && moduleType != it && moduleType.name?.startsWith(
+            it?.name?.substringBefore(
+                "_core"
+            )
+        )
+    }
+
+    internal fun sameLayerChildModule(
+        moduleType: ProjectModule.ModuleType
+    ): List<ProjectModule.ModuleType> = ProjectModule.ModuleType.values()
+        .filter {
+            moduleType != it && it.name?.startsWith(moduleType.name)
+        }
+
+    fun convertEnumName(project: Project): String = project.path
+        .replace(":", "_")
+
+    fun convertModulePath(moduleType: ProjectModule.ModuleType): String = moduleType.name
+        .replace("_", ":")
+
+    internal fun implAllModule(
+        dependencyHandler: DependencyHandler,
+        moduleType: ProjectModule.ModuleType
+    ) = ProjectModule.ModuleType.values().toSet()
+        .minus(moduleType).forEach {
+            dependencyHandler.impl(it)
+        }
+
+    internal fun DependencyHandler.impl(moduleType: ProjectModule.ModuleType) {
+        convertModulePath(moduleType).let { modulePath ->
+            println("structure:impl => $modulePath")
+            add(Libs.Props.Impl.prop, project(mapOf("path" to modulePath)))
+        }
+    }
+
+    internal fun DependencyHandler.api(moduleType: ProjectModule.ModuleType) {
+        convertModulePath(moduleType).let { modulePath ->
+            println("structure:api => $modulePath")
+            add(Libs.Props.Api.prop, project(mapOf("path" to modulePath)))
+        }
+    }
 }
