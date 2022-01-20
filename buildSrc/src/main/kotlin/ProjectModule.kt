4957e0fa4ee059836688194f1a84b96a6a6b69b9
narumichikubo <narumichi7373@gmail.com>
2022-01-21 00:50:10 +0900

    feat: 🎸 changed layerType/domainType enum definition to object

--- buildSrc/src/main/kotlin/ProjectModule.kt
+++ buildSrc/src/main/kotlin/ProjectModule.kt
@@ -1,45 +1,36 @@
 object ProjectModule {
 
-    enum class LayerType(val path: String, val isParent: Boolean = true) {
-        App(":app", true),
-        View(":presentation:view"),
-        ViewModel(":presentation:viewModel"),
-        UseCase(":domain:useCase"),
-        Entity(":domain:entity"),
-        Repository(":data:repository"),
-        Gateway(":data:gateway"),
+    /**
+     * モジュールのレイヤー一覧
+     */
+    enum class LayerType {
+        view,
+        viewModel,
+        useCase,
+        entity,
+        repository,
+        gateway,
     }
 
-    enum class DomainType(val path: String) {
-        Core(":core")
-    }
-
-    enum class OptionalDomainType(val path: String) {
-        GatewayLocal(":local"),
-        GatewayRemote(":remote")
+    /**
+     * ドメイン一覧
+     */
+    enum class DomainType {
+        core
     }
 
     enum class ModuleType(
-        val layerType: LayerType,
+        val layerType: LayerType? = null,
         val domainType: DomainType? = null,
-        val optionalDomainType: OptionalDomainType? = null,
     ) {
-        _app(LayerType.App),
-        _view(LayerType.View),
-        _viewCore(LayerType.View, DomainType.Core),
-        _viewModel(LayerType.ViewModel),
-        _viewModelCore(LayerType.ViewModel, DomainType.Core),
-        _useCase(LayerType.UseCase),
-        _useCaseCore(LayerType.UseCase, DomainType.Core),
-        _entity(LayerType.Entity),
-        _entityCore(LayerType.Entity, DomainType.Core),
-        _repository(LayerType.Repository),
-        _repositoryCore(LayerType.Repository, DomainType.Core),
-        _gateway(LayerType.Gateway),
-        _gatewayLocal(LayerType.Gateway, optionalDomainType = OptionalDomainType.GatewayLocal),
-        _gatewayRemote(LayerType.Gateway, optionalDomainType = OptionalDomainType.GatewayRemote),
+        _app(),
+        _presentation_view_core(LayerType.view, DomainType.core),
+        _presentation_viewModel_core(LayerType.viewModel, DomainType.core),
+        _domain_useCase_core(LayerType.useCase, DomainType.core),
+        _domain_entity_core(LayerType.entity, DomainType.core),
+        _data_repository_core(LayerType.repository, DomainType.core),
+        _data_gateway_local(LayerType.gateway),
+        _data_gateway_remote(LayerType.gateway),
     }
 
-    fun formatPath(modulePath: ProjectModule.ModuleType) =
-        modulePath.layerType.path + modulePath.domainType?.path + modulePath.optionalDomainType?.path
 }
---
+++ buildSrc/src/main/kotlin/script/ModuleExtension.kt
@@ -0,0 +1,4 @@
+package script
+
+class ModuleExtension {
+}
