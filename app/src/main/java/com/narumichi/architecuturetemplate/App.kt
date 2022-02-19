3cf08d2f9ad9732f52a094b016b05ce576836fad
narumichikubo <narumichi7373@gmail.com>
2022-02-19 14:06:24 +0900

    fix: 🐛 resolved compile error by add javapoet in buildsrc kts

--- app/src/main/java/com/narumichi/architecuturetemplate/App.kt
+++ app/src/main/java/com/narumichi/architecuturetemplate/App.kt
@@ -1,7 +1,9 @@
 package com.narumichi.architecuturetemplate
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App : Application() {
 
     override fun onCreate() {
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,4 +10,5 @@ repositories {
 dependencies {
     implementation("com.android.tools.build:gradle:7.0.4")
     implementation(kotlin("gradle-plugin","1.6.0"))
+    implementation("com.squareup:javapoet:1.13.0")
 }
