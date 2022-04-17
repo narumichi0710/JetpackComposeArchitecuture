e9c74e84c8a558fb94d5dbcb6fb9ad99750b0c5f
narumichikubo <narumichi7373@gmail.com>
2022-04-17 15:07:44 +0900

    feat: 🎸 add AuthActivity

---
+++ app/src/main/java/com/narumichi/architecuturetemplate/activity/AuthActivity.kt
@@ -0,0 +1,14 @@
+package com.narumichi.architecuturetemplate.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.narumichi.architecuturetemplate.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class AuthActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_auth)
+        }
+}
